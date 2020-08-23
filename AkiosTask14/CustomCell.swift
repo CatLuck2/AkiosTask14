0bfcb6f770308646cb259c089a139abe30184b58
猫吉 <freeaddress834@gmail.com>
2020-08-23 11:53:09 +0900

    fix
    Layout of UIImage by func - resize,creating empty UIImage

--- AkiosTask14/CustomCell.swift
+++ AkiosTask14/CustomCell.swift
@@ -22,14 +22,51 @@ class CustomCell: UITableViewCell {
     }
     
     func configure(text:String, keyCheck:Bool) {
+        let Resize:CGSize = CGSize.init(width: checkItemImage.frame.width, height:checkItemImage.frame.height)
         //チェックマークの画像を表示
         if keyCheck {
-            checkItemImage.image = UIImage(systemName: "checkmark")
+            let imageResize = UIImage(systemName: "checkmark")!.resize(size: Resize)
+            checkItemImage.image = imageResize
         } else {
-            checkItemImage.image = UIImage(systemName: "")
+            let myImage = UIImage.colorImage(color: UIColor.white, size: checkItemImage.frame.size, imageView: checkItemImage)
+            let imageResize = myImage.resize(size: Resize)
+            checkItemImage.image = imageResize
         }
         //チェックリストのテキストを表示
         checkItemLabel.text = text
     }
+    
+}
+
+extension UIImage {
+    
+    func resize(size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / size.width
+        let heightRatio = _size.height / size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+        
+        let resizedSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(resizedSize, false, 0.0) // 変更
+        draw(in: CGRect(origin: .zero, size: resizedSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage
+    }
+    
+    class func colorImage(color: UIColor, size: CGSize, imageView:UIImageView) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+
+        let rect = imageView.frame
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+
+        UIGraphicsEndImageContext()
+
+        return image
+   }
     
 }
